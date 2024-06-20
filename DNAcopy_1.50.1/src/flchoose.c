063444516882484f810509d53c7f4b4962efa6bf
siranli01 <108091445+siranli01@users.noreply.github.com>
2024-06-20 09:40:21 -0400

    Delete DNAcopy_1.50.1 directory

--- DNAcopy_1.50.1/src/flchoose.c
+++
@@ -1,4 +0,0 @@
-#include <R.h>
-#include <Rmath.h>
-/* Fortran function for log-combinations */
-double F77_SUB(flchoose)(double *n, double *k) { return lchoose(*n, *k); }
--- DNAcopy_1.50.1/src/fpnorm.c
+++
@@ -1,4 +0,0 @@
-#include <R.h>
-#include <Rmath.h>
-/* Fortran function for standard normal CDF */
-double F77_SUB(fpnorm)(double *x) { return pnorm(*x, 0, 1, 1, 0); }
